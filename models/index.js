812f8eb6096477b72cd92909c2d1807726b39cf3
hirwa-christian <christianatsuh@gmail.com>
2024-12-04 09:19:34 +0200

    fix cors

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ const {Sequelize,DataTypes}=require('sequelize')
 
 const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    port: process.env.PORT, // Use the port from the config
+    port: process.env.DB_PORT, // Use the port from the config
     dialect: process.env.DIALECT,
     logging: false, // Optional: Disable logging for cleaner output
     pool: {
